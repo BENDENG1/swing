c246ddabdfb7f3f05a8cc2c89fd43cd14a7de0fe
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-04-13 14:57:28 +0900

    feat : Activity,Fragment 템플릿 메소드 적용

---
+++ presentation/src/main/java/com/bendeng/presentation/base/BaseActivity.kt
@@ -0,0 +1,37 @@
+package com.bendeng.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class BaseActivity<B : ViewDataBinding>(
+    private val inflate: (LayoutInflater) -> B,
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflate(layoutInflater)
+        binding.lifecycleOwner = this
+        setContentView(binding.root)
+        initView()
+        initEventObserver()
+    }
+
+    abstract fun initView()
+    abstract fun initEventObserver()
+
+    fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
+        }
+    }
+}
---
+++ presentation/src/main/java/com/bendeng/presentation/base/BaseFragment.kt
@@ -0,0 +1,56 @@
+package com.bendeng.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes private val layoutRes: Int
+) : Fragment() {
+    private var _binding: B? = null
+    protected val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutRes, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initEventObserver()
+    }
+
+    fun LifecycleOwner.repeatOnStarted(block: suspend CoroutineScope.() -> Unit) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED, block)
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    abstract fun initView()
+
+    abstract fun initEventObserver()
+}
