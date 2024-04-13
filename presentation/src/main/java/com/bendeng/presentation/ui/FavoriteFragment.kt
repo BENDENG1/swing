e7bc9b16d9c9bbeee2c30ab0a5eb371d4331b585
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-04-13 15:22:49 +0900

    feat : MainActivity, FeedFragment, FavoriteFragment 생성

---
+++ presentation/src/main/java/com/bendeng/presentation/ui/FavoriteFragment.kt
@@ -0,0 +1,15 @@
+package com.bendeng.presentation.ui
+
+import com.bendeng.presentation.R
+import com.bendeng.presentation.base.BaseFragment
+import com.bendeng.presentation.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : BaseFragment<FragmentFavoriteBinding>(R.layout.fragment_favorite) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initEventObserver() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ presentation/src/main/java/com/bendeng/presentation/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bendeng.presentation.ui
+
+import com.bendeng.presentation.base.BaseActivity
+import com.bendeng.presentation.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun initView() {
+
+    }
+
+    override fun initEventObserver() {
+
+    }
+}
