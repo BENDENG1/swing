a1a1f85b9618efc70a4e559d13bd0b76a95ca62b
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2024-04-15 21:02:38 +0900

    feat : error response code에 따른 message분류
    unPlash API의 Common Status Code를 참고하여 message분류

--- data/src/main/java/com/bendeng/data/model/runRemote.kt
+++ data/src/main/java/com/bendeng/data/model/runRemote.kt
@@ -12,11 +12,24 @@ suspend fun <T> runRemote(block: suspend () -> Response<T>): BaseState<T> {
                 BaseState.Success(it)
             } ?: BaseState.Error("검색 결과 존재하지 않습니다.")
         } else {
-            val errorData =
-                Gson().fromJson(response.errorBody()?.string(), BaseState.Error::class.java)
-            BaseState.Error(errorData.message)
+            BaseState.Error(HttpStatus.getMessageForCode(response.code()))
         }
     } catch (e: Exception) {
         BaseState.Error(e.message.toString())
+    }
+}
+
+enum class HttpStatus(val code: Int, val message: String) {
+    BAD_REQUEST(400, "요청이 정확하지 않습니다.\n다시 확인하세요."),
+    UNAUTHORIZED(401, "현재 사용자는 인가되지 않은 사용자 입니다\n"),
+    FORBIDDEN(403, "요청의 횟수가 허용된 갯수를 초과하였습니다.\n나중에 시도하세요."),
+    NOT_FOUND(404, "찾을 수 없습니다.\n"),
+    INTERNAL_SERVER_ERROR(500, "서버의 문제가 생겼습니다.\n"),
+    SERVICE_UNAVAILABLE(503, "현재 서비스를 사용할 수 없습니다.\n");
+
+    companion object {
+        fun getMessageForCode(code: Int): String {
+            return values().find { it.code == code }?.message ?: "알수 없는 에러 발생"
+        }
     }
 }
